986c123ae0ececc386aa7776173a1390ce0dbcc2
roms <rusirusamaraweera749@gmail.com>
2023-04-29 15:34:26 +0530

    clear things up

--- components/Navbar.js
+++ components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
             </div>
             <Link href="/">Home</Link>
             <Link href="/about">About</Link>
-            <Link href="/contact">Contact</Link>
+            <Link href="/ninjas">Ninja List</Link>
         </nav>
      );
 }
